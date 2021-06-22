1c4a3cfa490ece0f9319beb05a24a6b720960675
castro-gabriel <gabrielcastromail@gmail.com>
2021-06-22 01:08:05 -0300

    instalacao do typeorm

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/test', (request, response) => {
+    return response.send("Hello NLW");
+})
+
+app.post('/test-post', (request, response) => {
+    return response.send('Hello NLW - POST');
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running!');
+});
