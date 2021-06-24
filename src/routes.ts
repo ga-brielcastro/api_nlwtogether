9affcaa77e07ce4cbbc05737c50a20e3ef075b05
castro-gabriel <gabrielcastromail@gmail.com>
2021-06-24 00:31:20 -0300

    Adicionado TagsController na routes e a rota /tags

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import Router from 'express';
 import { CreateUserController } from './controllers/CreateUserController';
+import { CreateTagController } from './controllers/CreateTagController';
 
 const router = Router();
 
-const createUserController = new CreateUserController()
+const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post('/users', createUserController.handle); //handle já está recebendo o request e response
-
+router.post('/tags', createTagController.handle);
 
 export { router };
